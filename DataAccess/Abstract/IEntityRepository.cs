187c66ee18b31eaa6c62c0615b1a5d24e436001f
sergennkeles <65495192+sergennkeles@users.noreply.github.com>
2021-02-05 00:36:24 +0300

    İnitial

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null);
+
+        void GetById(Expression<Func<TEntity, bool>> filter);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryDal.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryDal : ICarDal
+    {
+        public void Add(Car entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Car entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetById(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
