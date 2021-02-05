cf435f224399c146d9be74efd9a1918d7d8072b7
sergennkeles <65495192+sergennkeles@users.noreply.github.com>
2021-02-05 12:48:40 +0300

    InMemory

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
 
 namespace ConsoleUI
 {
@@ -6,7 +8,13 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            CarManager carManager = new CarManager(new InMemoryDal());
+
+            foreach (var car in carManager.GetAllCars())
+            {
+                Console.WriteLine(car.ModelYear);
+            }
         }
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryDal.cs
@@ -2,6 +2,7 @@
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -9,29 +10,42 @@ namespace DataAccess.Concrete.InMemory
 {
     public class InMemoryDal : ICarDal
     {
+        List<Car> _cars;
+
+        public InMemoryDal()
+        {
+            _cars = new List<Car> { 
+                new Car { Id = 1, BrandId = 2, ColorId = 2, ModelYear = "2019", DailyPrice = 199, Description = "Kiralık" },
+                new Car { Id = 2, BrandId = 3, ColorId = 1, ModelYear = "2018", DailyPrice = 299, Description = "Kiralık" }};
+        }
         public void Add(Car entity)
         {
-            throw new NotImplementedException();
+            _cars.Add(entity);
         }
 
         public void Delete(Car entity)
         {
-            throw new NotImplementedException();
+            _cars.Remove(entity);
         }
 
         public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            return _cars;
         }
 
-        public void GetById(Expression<Func<Car, bool>> filter)
+        public void GetById(Car entity, Expression<Func<Car, bool>> filter)
         {
-            throw new NotImplementedException();
+            _cars.Find(c => c.Id == entity.Id);
         }
 
         public void Update(Car entity)
         {
-            throw new NotImplementedException();
+            Car carUpdate = _cars.SingleOrDefault(c => c.Id == entity.Id);
+            carUpdate.BrandId = entity.BrandId;
+            carUpdate.ColorId = entity.ColorId;
+            carUpdate.ModelYear = entity.ModelYear;
+            carUpdate.DailyPrice = entity.DailyPrice;
+            carUpdate.Description = entity.Description;
         }
     }
 }
